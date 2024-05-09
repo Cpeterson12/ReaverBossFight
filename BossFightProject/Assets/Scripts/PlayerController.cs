5b12149fa3e91fc29bf5c79ba70b17b80a79338b
Caden Peterson <10867828@uvu.edu>
2024-05-09 16:12:14 -0600

    Revert "movement!"
    This reverts commit e79e80744dd0efb72c132dc802f4891d10e60e5b.

--- BossFightProject/Assets/Scripts/PlayerController.cs
+++ BossFightProject/Assets/Scripts/PlayerController.cs
@@ -7,17 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public FloatData moveSpeed;
-    public FloatData horizontal;
     public FloatData jumpSpeed;
     public PlayerInputActions playerControls;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-
-
+ 
+    private Vector2 moveDirection = Vector2.zero;
+    private Vector2 jumpDirection = Vector2.zero;
     private InputAction move;
     private InputAction jump;
-    private bool isFacingRight = true;
-    private bool hasDoubleJumped = false;
 
 
     private void Awake()
@@ -25,73 +21,29 @@ public class PlayerController : MonoBehaviour
         playerControls = new PlayerInputActions();
     }
 
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        jump = playerControls.Player.Jump;
+        move.Enable();
+        jump.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        jump.Disable();
+    }
+
     public void Update()
+{
+    moveDirection = move.ReadValue<Vector2>();
+    jumpDirection = jump.ReadValue<Vector2>();
+
+}
+
+    private void FixedUpdate()
     {
-        float speedMultiplier = IsGrounded() ? 1f : 0.7f; // Use full speed on the ground, halve the speed in the air
-        rb.velocity = new Vector2(horizontal.data * moveSpeed.data * speedMultiplier, rb.velocity.y);
-
-        if (!isFacingRight && horizontal.data > 0f)
-        {
-            Flip();
-        }
-        else if (isFacingRight && horizontal.data < 0f)
-        {
-            Flip();
-        }
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed.data, jumpDirection.y * jumpSpeed.data);
     }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed.data);
-        }
-
-        if (context.canceled && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-    }
-    
-    public void DoubleJump(InputAction.CallbackContext context)
-    {
-        if (context.performed && !IsGrounded() && !hasDoubleJumped)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed.data);
-            hasDoubleJumped = true;
-        }
-
-        if (context.canceled && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-    }
-
-    private bool IsGrounded()
-    {
-        bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-
-        if (isGrounded)
-        {
-            hasDoubleJumped = false;
-        }
-
-        return isGrounded;
-
-    }
-
-    private void Flip()
-    {
-        isFacingRight = !isFacingRight;
-        Vector3 localScale = transform.localScale;
-        localScale.x *= -1f;
-        transform.localScale = localScale;
-    }
-
-    public void Move(InputAction.CallbackContext context)
-    {
-        horizontal.data = context.ReadValue<Vector2>().x;
-    }
-
-    
 }
